133117df2b5c500f9fe1784fd22b12eb60358857
Audrey Norvez <audrey.norvez@gmail.com>
2024-11-18 14:56:53 +0100

    feature: identification JWT et protection de la page réservation

--- jwt.js
+++ jwt.js
@@ -1,8 +1,43 @@
 const jwt = require('jsonwebtoken');
-const secret = 'mysecret'; //en réalité doit être complexe avec une chaîne de 32 caractères
+const fs = require('fs');
+
+const EXPIRATION = '1 day';
+const SECRET = fs.readFileSync('private.key');
 
 // TODO : Déclarer un middleware qui vérifiera le jwt retourné par le client
 // Il sera réutilisé pour toutes les routes protégées 
+
+const extractBearerToken = headervalue => {
+
+    if(typeof headervalue !== 'string'){
+        return false;
+    }
+    const matches = headervalue.match(/(bearer)\s+(\S+)/i);
+    return matches && matches[2];
+}
+
+
+const checkTokenMiddleware = (req, res, next) => {
+
+    //Le JWT est placé dans le header Authorization
+    //Recupere le jwt envoyé par le client
+    const token  = req.headers.authorization && extractBearerToken(req.headers.authorization);
+    
+    if(!token){
+        return res.status(401).json({"msg":"Vous n'êtes pas authorisé à acceder à cette ressource."})
+    }
+    
+    //vérification du jwt
+    jwt.verify(token, SECRET, (err, decodedToken) => {
+        //la vérification a échoué
+        if(err){
+            return res.status(401).json({"msg":"Vous n'êtes pas authorisé à acceder à cette ressource."})
+        }
+        // la vérification a réussi
+        console.log("Token vérifié!")
+    })
+    next();
+}
 
 /**
 ** Retourne un jwt signé avec une date d'expiration
@@ -11,16 +46,16 @@ const secret = 'mysecret'; //en réalité doit être complexe avec une chaîne d
 * @returns
 */
 
-function createJWT(login, isAdmin, expiration = '1 day'){
+function createJWT(login, isAdmin, expiration = EXPIRATION){
 
     return jwt.sign({
         login: login,
         isAdmin: isAdmin,
     },
-    secret, {
+    SECRET, {
         expiresIn:expiration
     }
     );
 }
 
-module.exports = {createJWT}
+module.exports = {createJWT, checkTokenMiddleware}
--- routes/reservations.js
+++ routes/reservations.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 var hal = require('../hal');
+var { checkTokenMiddleware } = require("../jwt");
 
-/* GET page (protégée) réservations d'un concert. */
-router.get('/concert/:id(\\d+)/reservation', function(req, res, next) {
-  res.status(200).json(db.reservations);
-});
+/** 
+* GET page (protégée) réservations d'un concert.
+* le middleware checkTokenMiddleware est appelé, puis celui qui est déclanché si next()
+**/
+router.get(
+  '/concerts/:id(\\d+)/reservation', 
+  checkTokenMiddleware, 
+  function(req, res, next) {
+    console.log("page des réservations")
+  }
+);
 
 module.exports = router;
